cf4a9337455ad0008f714d8ae7183773bc574810
habibur nabi <habiburnabiarafat@gmail.com>
2023-11-23 18:06:26 +0600

    project setup done without the data model or pattern

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import { Server } from "http"
 import app from "./app"
-const PORT = 3000
+import mongoose from "mongoose";
+import config from "./app/config";
 
-let server: Server;
-
-async function bootstrap () {
+async function main () {
     try {
-        
-        server = app.listen(PORT)
-        console.log(`Application listening on port ${PORT}`)
+        await mongoose.connect(config.database_url as string);
+         app.listen(config.port,()=>{
+
+            console.log(`Application listening on port ${config.port}`)
+        })
     } catch (error) {
         console.error(error)
     }
 }
 
-bootstrap()
+main()
